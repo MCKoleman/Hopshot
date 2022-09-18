d5b084866a460cf38ba2d6f954424db7c709777c
RobertKilkenny <robertkilkenny@ufl.edu>
2022-09-17 21:53:15 -0400

    Added Firing mechanism
    -Shoots on sight of player
    -Stops when they can't see anymore
    -Bullets added and damage enemies+ player
    -Bullet trail
    
    NEED TO ADDRESS
    -Bullet destruction on impact
    -Speed?

--- Assets/Scripts/Characters/Turret_Logic.cs
+++ Assets/Scripts/Characters/Turret_Logic.cs
@@ -10,10 +10,37 @@ public class Turret_Logic : MonoBehaviour
     [SerializeField]
     private Target_Light led;
 
+    [SerializeField]
+    private GameObject bullet;
+
+    [SerializeField]
+    private Transform spawn;
+
+    [SerializeField]
+    private float bulletSpeed;
+    private float correctPath = 1.0f;
+    float nextShot = 0.0f;
+
+    [SerializeField]
+    private bool faceLeft;
+    private bool fire;
+
+    private void Update()
+    {
+        if(fire && Time.time > nextShot)
+        {
+            Shoot();
+            nextShot = Time.time + .5f;
+        }
+    }
+
     private void OnEnable()
     {
         gun.StartFire += Wake;
         gun.StopFire += Sleep;
+
+        if(faceLeft)
+            correctPath = -1.0f;
     }
 
     private void OnDisable()
@@ -25,11 +52,19 @@ public class Turret_Logic : MonoBehaviour
     private void Wake()
     {
         led.SetColor(Color.red);
+        fire = true;
     }
 
     private void Sleep()
     {
         led.SetColor(Color.green);
+        fire = false;
     }
 
+    void Shoot()
+    {
+        Vector2 direction = (gun.transform.position - spawn.transform.position) * correctPath;
+        GameObject loadShot = Instantiate(bullet, spawn.position, Quaternion.identity);
+        loadShot.GetComponent<Rigidbody2D>().AddForce(direction * bulletSpeed);
+    }
 }
